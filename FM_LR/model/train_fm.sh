46b929a649a264310ab93d4bdf2b58a6b7160a7c
zhangqiang21 <zhangqiang21@51ganji.com>
2017-11-05 11:16:40 +0800

    train FM with libfm.

--- FM_LR/model/train_fm.sh
+++ FM_LR/model/train_fm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # 用libFM训练FM模型（SGD）
-../tools/libfm -task c -train ../data/train_data.libfm -test ../data/test_data.libfm -dim '1,1,8'
--method sgd -learn_rate 0.01 -regular 0 -init_stdev 0.001 -iter 1000 -out ../train/out.libfm -rlog ../train/log.txt
+# 在公司机器上训练
+../tools/libfm/bin/libFM -task r -train train_data.libfm -test test_data.libfm -dim '1,1,8' -method sgd -learn_rate 0.01
+-regular 0 -init_stdev 0.001 -iter 100 -save_model fm.model -out result.txt -rlog log.txt
